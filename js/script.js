0b78a752ab50261381a6308452ddc956c57d17ee
Reaien <ivanduarteherrera@gmail.com>
2023-04-24 01:14:22 -0400

    finalizada validacion basica formulario login js

--- js/script.js
+++ js/script.js
@@ -12,10 +12,9 @@ const selects = document.querySelectorAll("#formulario select")
 
 const expresiones = {
 	rut: /^[0-9\_\-\.]{11,12}$/, // Letras, numeros, guion y guion_bajo
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	password: /^.{4,12}$/, // 4 a 12 digitos.
+	nombre: /^[a-zA-ZÀ-ÿ\s]{4,25}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^[0-9\_\-\.]{8,12}$/, // 4 a 12 alfanumericos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,14}$/, // 7 a 14 numeros.
     edad: /^\d{1,2}$/, // 1 a 2 digitos.
     genero: /^[a-zA-ZÀ-ÿ\s]{2,6}$/ // caracteres de genero
 };
@@ -55,15 +54,15 @@ const validarFormulario = (e) => {
                 document.querySelector('#grupo__appaterno .formulario__input-error').classList.add('formulario__input-error--activo');
             }
             break;
-        case "apellidom":
-            if (expresiones.nombre.test(e.target.value)) {
-                document.getElementById('grupo__apmaterno').classList.remove('formulario__grupo-incorrecto');
-                document.getElementById('grupo__apmaterno').classList.add('formulario__grupo-correcto');
-                document.querySelector('#grupo__apmaterno .formulario__input-error').classList.remove('formulario__input-error--activo');
+        case "correo":
+            if (expresiones.correo.test(e.target.value)) {
+                document.getElementById('grupo__correo').classList.remove('formulario__grupo-incorrecto');
+                document.getElementById('grupo__correo').classList.add('formulario__grupo-correcto');
+                document.querySelector('#grupo__correo .formulario__input-error').classList.remove('formulario__input-error--activo');
             }else{
-                document.getElementById('grupo__apmaterno').classList.remove('formulario__grupo-correcto');
-                document.getElementById('grupo__apmaterno').classList.add('formulario__grupo-incorrecto');
-                document.querySelector('#grupo__apmaterno .formulario__input-error').classList.add('formulario__input-error--activo');
+                document.getElementById('grupo__correo').classList.remove('formulario__grupo-correcto');
+                document.getElementById('grupo__correo').classList.add('formulario__grupo-incorrecto');
+                document.querySelector('#grupo__correo .formulario__input-error').classList.add('formulario__input-error--activo');
             }
             break;
         case "edad":
@@ -89,19 +88,52 @@ const validarFormulario = (e) => {
                 document.querySelector('#grupo__genero .formulario__input-error').classList.remove('formulario__input-error--activo'); 
             }
             break;
+        case "password":
+            if (expresiones.password.test(e.target.value)) {
+                document.getElementById('grupo__password').classList.remove('formulario__grupo-incorrecto');
+                document.getElementById('grupo__password').classList.add('formulario__grupo-correcto');
+                document.querySelector('#grupo__password .formulario__input-error').classList.remove('formulario__input-error--activo');
+            }else{
+                document.getElementById('grupo__password').classList.remove('formulario__grupo-correcto');
+                document.getElementById('grupo__password').classList.add('formulario__grupo-incorrecto');
+                document.querySelector('#grupo__password .formulario__input-error').classList.add('formulario__input-error--activo');
+            }
+            validarPassword2();
+            break; 
+        case "password2":
+            validarPassword2();    
+            break;       
     }
 };
 
+//lector for each de todos los selects en form para activar el listener en validarFormulario
 selects.forEach((select) =>{
     select.addEventListener("keyup", validarFormulario);
     select.addEventListener("blur", validarFormulario);
 });
-
+//lector for each de todos los inputs en form para activar el listener en validarFormulario
 inputs.forEach((input) =>{
     input.addEventListener("keyup", validarFormulario);
     input.addEventListener("blur", validarFormulario);  
 });
-
+//prevenir el envio en el btn tipo submit
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
 });
+
+
+//funcion para validar pass
+const validarPassword2 = () =>{
+    const inputPassword1 = document.getElementById('password');
+    const inputPassword2 = document.getElementById('password2');
+if (inputPassword1.value !== inputPassword2.value) {
+    document.getElementById('grupo__password2').classList.remove('formulario__grupo-correcto');
+    document.getElementById('grupo__password2').classList.add('formulario__grupo-incorrecto');
+    document.querySelector('#grupo__password2 .formulario__input-error').classList.add('formulario__input-error--activo');
+}else{
+    document.getElementById('grupo__password2').classList.remove('formulario__grupo-incorrecto');
+    document.getElementById('grupo__password2').classList.add('formulario__grupo-correcto');
+    document.querySelector('#grupo__password2 .formulario__input-error').classList.remove('formulario__input-error--activo');
+}
+
+};
